38b668323c3d1988874609330baba3f0010f28f2
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-05-04 04:19:25 +0300

    created profile.php

---
+++ backend/profile.php
@@ -0,0 +1,22 @@
+<?php
+
+include("db_info.php");
+
+$id = $_GET["user_id"];
+
+$query = $mysqli->prepare("SELECT * FROM Trainers WHERE TrainerID = ?");
+$query->bind_param("i", $id);
+$query->execute();
+
+$array = $query->get_result();
+
+$response = [];
+
+while($course = $array->fetch_assoc()){
+    $response[] = $course;
+}
+
+$json_response = json_encode($response);
+echo $json_response;
+
+?>

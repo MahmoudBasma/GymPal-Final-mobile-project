e237d62d282f62565b72e10f9c36e935c3748bb5
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-05-04 09:22:51 +0300

    modal attempt successs

--- frontend/src/app/pages/edit-profile-pop/edit-profile-pop.page.ts
+++ frontend/src/app/pages/edit-profile-pop/edit-profile-pop.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input} from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-edit-profile-pop',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfilePopPage implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtr: ModalController, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  async close() {
+    this.modalCtr.dismiss();
   }
 
 }
--- frontend/src/app/tab4/tab4.page.ts
+++ frontend/src/app/tab4/tab4.page.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { computeStackId } from '@ionic/angular/directives/navigation/stack-utils';
 import { ProfileService, Profile } from '../apis/profile.service';
 import { QualificationsService , Qualification} from '../apis/qualifications.service';
+import { ModalController } from '@ionic/angular';
+import { EditProfilePopPage } from '../pages/edit-profile-pop/edit-profile-pop.page';
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
   styleUrls: ['tab4.page.scss']
 })
 export class Tab4Page {
+  modalDataResponse: any;
+
   profile: Profile[];
   qualifications: Qualification[];
 
-  constructor(private Pservice: ProfileService, private Qservice: QualificationsService) {}
+  constructor(private Pservice: ProfileService, private Qservice: QualificationsService, 
+    public modalCtrl: ModalController) {}
 
   ngOnInit(){
     this.Pservice.getFullProfile().subscribe(response => {
@@ -22,5 +27,23 @@ export class Tab4Page {
       this.qualifications = response;
       console.log(this.qualifications);
     })
+  }
+
+  async initModal() {
+    const modal = await this.modalCtrl.create({
+      component: EditProfilePopPage,
+      componentProps: {
+        'name': 'The Winter Soldier'
+      }
+    });
+
+    modal.onDidDismiss().then((modalDataResponse) => {
+      if (modalDataResponse !== null) {
+        this.modalDataResponse = modalDataResponse.data;
+        console.log('Modal Sent Data : '+ modalDataResponse.data);
+      }
+    });
+
+    return await modal.present();
   }
 }

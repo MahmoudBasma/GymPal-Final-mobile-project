40298ac16046ed4c8969f26e65fbae6fcb1c0235
Mahmoud Basma <mahmoud.basma@lau.edu>
2022-05-05 21:03:43 +0300

    Update getCustomers.php

--- backend/getCustomers.php
+++ backend/getCustomers.php
@@ -5,7 +5,10 @@ include("db_info.php");
 
 $id = $_GET["user_id"];
 
-$query = $mysqli->prepare("SELECT * FROM Fees WHERE TrainerID = ? ORDER BY Date DESC");
+$query = $mysqli->prepare(
+    "SELECT C.Name name, J.TrainerID, C.customerID
+    FROM `customers` C, `customer-trainer` J
+    WHERE C.customerID = J.customerID and J.trainerID=?;");
 $query->bind_param("i", $id);
 $query->execute();
 
@@ -13,20 +16,10 @@ $array = $query->get_result();
 
 $response = [];
 
-while($fee = $array->fetch_assoc()){
-    $response[] = $fee;
+while($customer = $array->fetch_assoc()){
+    $response[] = $customer;
 }
 
-$query = $mysqli->prepare("SELECT C.Name, J.TrainerID
-FROM `customers` C, `customer-trainer` J
-WHERE C.customerID = J.customerID and J.trainerID=?;");
-$query->bind_param("i", $id);
-$query->execute();
-
-$array = $query->get_result();
-while($fee = $array->fetch_assoc()){
-    $response[] = $fee;
-}
 
 $json_response = json_encode($response);
 echo $json_response;
